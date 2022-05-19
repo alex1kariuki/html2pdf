18ead647b626bab8b66b369ee6463cb021649dd8
alexkariuki <alexkariuki2@gmail.com>
2022-05-19 21:06:02 +0300

    Added array qrs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,26 @@ export class AppComponent {
   title = 'html2pdf';
   name: string = 'big man';
   width: number = 300;
-
+  myArray: any[];
 
   constructor() {
-
+    this.myArray = [
+      1, 2, 3, 4, 5, 1, 2, 3, 4, 5
+    ]
   }
   async printQr() {
     console.log(this.name);
     // Choose the element that our invoice is rendered in.
     const element = document.getElementById("element-to-print");
     var opt = {
-      margin:       1,
-      filename:     'output.pdf',
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+      margin: 1,
+      filename: 'output.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 5 },
+      width: 500,
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
     };
-     
+
     // New Promise-based usage:
     html2pdf().from(element).set(opt).save();
   }
